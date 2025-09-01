80830e85b8ae5ba3ce73e57abf900516357ac4db
Dade1991 <dade91@msn.com>
2025-09-01 11:18:57 +0200

    PJT Initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import CustomNavbar from "./components/CustomNavbar"
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <CustomNavbar />
         <Routes>
           <Route></Route>
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
