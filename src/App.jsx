97e7154756ccb7d63f4531a8980b68f41a0bb6fa
Dade1991 <dade91@msn.com>
2025-09-01 16:29:09 +0200

    Custom navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import CustomNavbar from "./components/CustomNavbar"
+import "./components/CustomNavbar.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 function App() {
   return (
