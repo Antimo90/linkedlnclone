2d7d658e233c9fd6d032d9a2c0c5b83e7f29dfef
Anna De Lise <anna_delise@libero.it>
2025-09-02 09:45:39 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import CustomNavbar from "./components/CustomNavbar";
 import "./components/CustomNavbar.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import OtherUsersTest from "./components/OtherUsersTest";
-import Profile from "./components/Profile";
+import Footer from "./components/Footer";
+import SuperContainer from "./components/SuperContainer";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <CustomNavbar />
-        <Routes>
-          <Route path="/" element={<Profile />} />
-          <Route path="/user/:userId" element={<OtherUsersTest />} />
-        </Routes>
+       <BrowserRouter>
+       <CustomNavbar />
+                <Routes>
+                  <Route path="/" element={<SuperContainer /> } />
+                  <Route path="/user/:userId" element={<OtherUsersTest />} />
+                </Routes>
+          <Footer />
       </BrowserRouter>
     </>
   );
